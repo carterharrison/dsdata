b461b973f3e6d8989974797fb76105b1d49bea47
Carter Harrison <carterjharrison@gmail.com>
2020-03-22 13:46:39 -0700

    move datasheets

--- main.go
+++ main.go
@@ -13,29 +13,27 @@ func main () {
 	}
 
 	r := NewReader(file)
+	//markers := make(map[string]int)
 
 	for r.HasNext() {
 		sheet := r.Next()
 
-		//fmt.Println(sheet.BasicMetadata["DESIGNATION"])
-		//fmt.Println(sheet.BasicMetadata["DESIGNATION"] + "  ____   " +sheet.NewSurveyControl[0].Value)
-
 		nums := getNumbersFromString(sheet.NewSurveyControl[0].Value)
 		lat := DegreesMinutesSeconds(nums[0], nums[1], nums[2])
 		lng := DegreesMinutesSeconds(nums[3], nums[4], nums[5]) * - 1
-
-		fmt.Println(sheet.BasicMetadata["DESIGNATION"], lat, lng)
-
-
-		//res, err := json.Marshal(sheet)
+		name := sheet.BasicMetadata["DESIGNATION"]
+		//fmt.Println(sheet.Monumentation["_MARKER"] )
+		//markers[sheet.Monumentation["_MARKER"]]++
 		//
-		//if err != nil {
-		//	panic(err)
-		//}
-		//
-		//fmt.Println(string(res))
+		if sheet.Monumentation["_MARKER"] == "" {
+			fmt.Println(name, lat, lng, sheet.Monumentation["_MARKER"])
+
+		}
 
 	}
+
+	//v, _ := json.MarshalIndent(markers, "", "      ")
+	//fmt.Println(string((v)))
 }
 
 func DegreesMinutesSeconds (deg float64, min float64, sec float64) float64 {
